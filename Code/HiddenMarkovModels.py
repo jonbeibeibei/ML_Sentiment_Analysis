74bfccc9c046b651116735045848d9222fb3b8cf
Ruth <ruthwongny@gmail.com>
2017-11-11 20:12:15 +0800

    reading, modifying test data and attempt at sentiment analysis

--- Code/HiddenMarkovModels.py
+++ Code/HiddenMarkovModels.py
@@ -6,6 +6,36 @@
 import os
 import sys
 
+class Main:
+    def __init__(self):
+        self.states = ['O', 'B-positive', 'B-neutral', 'B-negative', 'I-positive', 'I-neutral', 'I-negative']
+        self.output = []
+
+    def simple_sentiment_analysis(self):
+        """
+        Sentiment analysis based on emission parameters only
+        Saves file 'dev.out'
+        :return: none
+        """
+        train_path = '../Datasets/Demo/train'
+        training_set = read_training_set(train_path)
+        training_set.all_emission_params()
+        emission = get_emission_params()
+        train_words = training_set.get_words()
+
+        test_path = '../Datasets/Demo/dev.in'
+        test_set = read_test_set(test_path)
+        test_set.modify_test_data(train_words)
+        test_words = test.
+        for tweet in test_set:
+            for i in range(0, tweet.getSize()):
+                word = tweet.get_x()[i]
+                probability = 0.0
+                for state in self.states:
+                    if emission[(word, state)]
+
+                emission[(word, 'O')]
+                emission[(word, 'B-positive')]
 
 class TweetSet:
     """
@@ -16,6 +46,7 @@ class TweetSet:
         self.tweets = []
         self.emission = {}
         self.pairs = []
+        self.words = []
         self.k = 3
 
     def set_k(k):
@@ -66,7 +97,7 @@ class TweetSet:
         """
         self.emission[(x, y)] = float(self.count_y_to_x(x, y)) / float(self.count_total_y(y))
 
-    def modify_data(self, k):
+    def modify_train_data(self, k):
         """
         Replace words that appear less than k times in training set with #UNK#
         :return: none
@@ -77,14 +108,30 @@ class TweetSet:
                     self.pairs.append(("#UNK#", tweet.get_y()[i]))
                 else:
                     self.pairs.append((tweet.get_x()[i], tweet.get_y()[i]))
+                    if (tweet.get_x()[i] not in self.words):
+                        self.words.append(tweet.get_x()[i])
 
+    def get_words(self):
+        return self.words
+
+    def modify_test_data(self, train_words):
+        """
+        Replace words from test that don't appear in train set with #UNK#
+        :return: none
+        """
+        for tweet in self.tweets:
+            for i in range(tweet.getSize()):
+                if (self.words not in train_words):
+                    self.words.append("#UNK#")
+                else:
+                    self.pairs.append((tweet.get_x()[i], tweet.get_y()[i]))
 
     def all_emission_params(self):
         """
         Iterate through all word/label pairs to populate emission parameters list
         :return: none
         """
-        self.modify_data(self.k)
+        self.modify_train_data(self.k)
         for p in self.pairs:
             self.add_emission_params(p[0], p[1])
 
@@ -170,12 +217,12 @@ class HiddenMarkovModel:
         return self.y
 
 
-def read_training_set():
+def read_training_set(path):
     """
     Reads in training data as provided
     """
     main_path = os.path.dirname(__file__)
-    training_path = os.path.join(main_path,'../Datasets/Demo/train')
+    training_path = os.path.join(main_path, path)
     training = open(training_path, encoding='utf-8').read().split('\n\n')
     del training[-1]    # because the last tweet ends with 3 '\n'
     print('Read training file with ' + str(len(training)) + ' tweets')
@@ -194,6 +241,37 @@ def read_training_set():
             if len(j) > 0:
                 new_tweet.add_x(j.split(" ")[0])
                 new_tweet.add_y(j.split(" ")[1])
+
+
+        if new_tweet.getSize() > 0:
+            new_set.add_tweet(new_tweet)
+
+    print("done!")
+    return new_set
+
+def read_test_set(path):
+    """
+    Reads in test data as provided
+    """
+    main_path = os.path.dirname(__file__)
+    training_path = os.path.join(main_path, path)
+    training = open(training_path, encoding='utf-8').read().split('\n\n')
+    del training[-1]    # because the last tweet ends with 3 '\n'
+    print('Read test file with ' + str(len(training)) + ' tweets')
+
+    new_set = TweetSet()
+
+    count = 0
+    for i in training:
+        # print("i: " + i )
+        count = count + 1
+        print(count)
+        new_tweet = HiddenMarkovModel()
+
+        for j in i.split('\n'):
+            # print(j.split(" "))
+            if len(j) > 0:
+                new_tweet.add_x(j)
 
 
         if new_tweet.getSize() > 0:
