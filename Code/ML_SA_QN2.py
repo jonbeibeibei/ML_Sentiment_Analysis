3cea5fea883817b0b0be8ce74a3a32375b38ce16
Ruth <ruthwongny@gmail.com>
2017-11-24 12:07:29 +0800

    Qn2 Done

--- Code/ML_SA_QN2.py
+++ Code/ML_SA_QN2.py
@@ -83,128 +83,65 @@ def count(training_data, k):
             
     return emission_count, y_count, x_count
 
-def emissions(x, y, training_data):
+def emissions(x, y, emission_count, y_count):
+    """
+    Getting emission parameters of x and y
+    Params emission_count and y_count taken from initial run of count()
+    :returns: float probability
+    """
+    try:
+        return float(emission_count[(y,x)]/y_count[y])
+    
+    except KeyError:
+        return 0.0
 
-    emission_count = 0
-    # x_count = 0
-    y_count = 0
-
-    for sentence in training_data:
-        for i in range(len(sentence)):
-            curr_sentence = sentence[i].split(' ')
-            curr_x = curr_sentence[0]
-            curr_y = curr_sentence[1]
-            # if curr_x == x:
-            #     x_count += 1
-            if curr_y == y:
-                y_count += 1
-                if curr_x == x:
-                    emission_count += 1
-
-    # if x_count == 0:
-    #     return float(1/y_count)
-
-    return float(emission_count/y_count)
-
-def get_optimal_y(x, training_data):
+def get_optimal_y(x, emission_count, y_count):
     optimum_y_prob = 0
     optimum_y = ''
     for state in states:
-        y_prob = emissions(x,state,training_data)
+        y_prob = emissions(x,state,emission_count,y_count)
         if y_prob > optimum_y_prob:
             optimum_y_prob = y_prob
             optimum_y = state
 
     return optimum_y
 
-def simple_sentiment_analysis(training_path, test_path,output_path):
+def simple_sentiment_analysis(training_path, test_path, output_path):
 
     optimal_y_dict = {}
 
-    trainFile = read_in_file(training_path)
-    train_words, modified_training_data = modify_train_data(3,trainFile)
-    print('done modifying training_data')
+    train_data = read_in_file(training_path)
+    print('done reading training file')
+    emission_count, y_count, x_count = count(train_data, 3)
+    print('done counting x, y, emissions')
 
-    testFile = read_in_file(test_path)
-    modified_test_data = modify_test_data(train_words, testFile)
-    print('done modifying test_data')
+    test_data = read_in_file(test_path)
+    print('done reading test file')
 
     main_path = os.path.dirname(__file__)
     save_path = os.path.join(main_path, output_path)
     with codecs.open(os.path.join(save_path,'dev.p2.out'), 'w', 'utf-8') as file:
-        for sentence in range(len(modified_test_data)):
-            for word in range(len(modified_test_data[sentence])):
-                if modified_test_data[sentence][word] in optimal_y_dict.keys():
-                    optimum_y = optimal_y_dict[modified_test_data[sentence][word]]
+        for sentence in test_data:
+            for word in sentence:
+                # To check if word in test data appears in training data
+                if word not in x_count:
+                    mod_word = '#UNK#'
                 else:
-                    optimum_y = get_optimal_y(modified_test_data[sentence][word], modified_training_data)
-                    optimal_y_dict[modified_test_data[sentence][word]] = optimum_y
-                output = testFile[sentence][word] + ' ' + optimum_y + '\n'
+                    mod_word = word
+                # To calculate best label
+                if mod_word in optimal_y_dict:
+                    optimum_y = optimal_y_dict[mod_word]
+                else:
+                    optimum_y = get_optimal_y(mod_word, emission_count, y_count)
+                    optimal_y_dict[mod_word] = optimum_y
+                output = word + ' ' + optimum_y + '\n'
                 file.write(output)
             file.write('\n')
 
     print('Done!')
     file.close()
 
-
-def modify_train_data(k, training_data):
-    """
-    Replace words that appear less than k times in training set with #UNK#
-    Appends word to a list for test data modification
-    :returns: none
-    """
-    #Get dictionary of all the words contained in training set
-    word_dict = {}
-    for sentence in training_data:
-        for i in range(len(sentence)):
-            curr_sentence = sentence[i].split(' ')
-            curr_x = curr_sentence[0]
-            if curr_x not in word_dict:
-                word_dict[curr_x] = 0
-            else:
-                word_dict[curr_x] += 1
-
-    # print(word_dict)
-    # Clone the training data
-    print('copying the train_data out...')
-    modified_training_data = copy.deepcopy(training_data)
-    output_word_dict = word_dict.copy()
-    print('copied!')
-    #Iterate over all words and check values, if < k, replace with "#UNK#"
-    for key, value in word_dict.items():
-
-        if value < k:
-            for sentence in modified_training_data:
-                for i in range(len(sentence)):
-                    curr_sentence = sentence[i].split(' ')
-                    curr_x = curr_sentence[0]
-                    curr_y = curr_sentence[1]
-                    if key == curr_x:
-                        sentence[i] = '#UNK#' + ' ' + curr_y
-                        output_word_dict.pop(key,None)
-
-    return output_word_dict, modified_training_data
-
-def modify_test_data(train_words, test_data):
-    """
-    Replace words that do not appear in train_words
-    Appends word to a list for test data modification
-    :returns: none
-    """
-
-    print('copying the test_data out...')
-    modified_test_data = copy.deepcopy(test_data)
-    print('copied!')
-
-    for sentence in modified_test_data:
-        for i in range(len(sentence)):
-            if sentence[i] not in train_words.keys():
-                sentence[i] = '#UNK#'
-
-
-    return modified_test_data
-
-# simple_sentiment_analysis('../Datasets/SG/train','../Datasets/SG/dev.in','../Datasets/SG')
-trainFile = read_in_file('../Datasets/SG/train')
-emission_count, y_count, x_count = count(trainFile, 3)
-print(emission_count)
+simple_sentiment_analysis('../Datasets/SG/train','../Datasets/SG/dev.in','../Datasets/SG')
+# trainFile = read_in_file('../Datasets/SG/train')
+# emission_count, y_count, x_count = count(trainFile, 3)
+# print(x_count)
